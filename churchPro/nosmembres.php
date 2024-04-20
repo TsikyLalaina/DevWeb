d3a0f8c39cb38404040f109f761a919d4f129d70
TsikyLalaina <125742307+TsikyLalaina@users.noreply.github.com>
2024-04-20 17:51:45 +0300

    Update nosmembres.php

--- churchPro/nosmembres.php
+++ churchPro/nosmembres.php
@@ -19,7 +19,7 @@
                 <ul>
                     <li>
                         <form action="" class="top-search-bar">
-                            <input type="text " placeholder="type to search..." name="q">
+                            <input type="text " placeholder="type to search..." name="q" class="livesearch">
                             <button type="submit"><i class="fa-solid fa-magnifying-glass fa-2xl"></i></button>
                         </form>
                     </li>
@@ -32,7 +32,7 @@
         </div>
         <section class="banner">
             <form action="" class="search-bar">
-                <input type="text " placeholder="type to search..." name="q">
+                <input type="text " placeholder="type to search..." name="q" class="livesearch">
                 <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
             </form>
         </section>
@@ -50,32 +50,7 @@
                 </div>
                 <div class="row row2">
                     <?php
-                        $query = "select * from members";
-                        $result = mysqli_query($con, $query);
-                        if($result){
-                            if($result && mysqli_num_rows($result) > 0){
-                                while($member_data = mysqli_fetch_assoc($result)){
-                                    echo '<div class="column">
-                                            <input type="checkbox" class="select-card" data-memberid="' . $member_data['studentid'] . '">
-                                            <div class="imgBox">
-                                                <img src="' . $member_data['image'] . '" alt="profile">
-                                            </div>
-                                            <div class="details">
-                                                <h3>' . $member_data['fname'] . '<br><span>' . $member_data['position'] . '</span></h3>
-                                                <ul>
-                                                    <li><a href="' . $member_data['facebook'] . '" target="_blank"><i class="fa-brands fa-facebook-f"></i></a></li>
-                                                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                                                    <li><a href="#"><i class="fa-brands fa-google-plus-g"></i></a></li>
-                                                    <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
-                                                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>';
-                                }                                  
-                            }else{echo '<h1>Aucun membre pour le moment</h1>';}
-                        }else{
-                            echo "Failed to fetch informations";
-                        }
+                        include("displaymembers.php");
                     ?>
                 </div>
         </section>
